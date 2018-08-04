fe6808b5960314ba5fa7a86af13199d2479653a1
Robby Grossman <robby@freerobby.com>
2018-08-04 19:06:07 -0400

    Simplify variable settings for Raspbian release; upgrade to Debian stretch.

--- flash.sh
+++ flash.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env sh
 
-RASPBIAN_RELEASE=2016-11-25-raspbian-jessie-lite
-RASPBIAN_URL=http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2016-11-29/$RASPBIAN_RELEASE.zip
+RASPBIAN_URL=http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-06-29/2018-06-27-raspbian-stretch-lite.zip
+RASPBIAN_ARCHIVE=$(echo $RASPBIAN_URL | sed 's:.*/\(.*\):\1:')
+RASPBIAN_IMAGE=$(echo $RASPBIAN_ARCHIVE | sed 's:\(.*\)\.zip:\1.img:')
 
-if [ -e ./tmp/$RASPBIAN_RELEASE.img ]
+if [ -e ./tmp/$RASPBIAN_IMAGE ]
 then
   echo "Raspbian image found."
 else
   echo "Raspbian image not found."
 
-  if [ -e ./tmp/$RASPBIAN_RELEASE.zip ]
+  if [ -e ./tmp/$RASPBIAN_ARCHIVE ]
   then
     echo "Raspbian archive found."
   else
     echo "Raspbian archive not found. Downloading latest..."
     mkdir -p ./tmp
-    curl -L -o ./tmp/$RASPBIAN_RELEASE.zip $RASPBIAN_URL
+    curl -L -o ./tmp/$RASPBIAN_ARCHIVE $RASPBIAN_URL
   fi
 
   echo "Extracting Raspbian archive..."
-  unzip ./tmp/$RASPBIAN_RELEASE.zip -d ./tmp
+  unzip ./tmp/$RASPBIAN_ARCHIVE -d ./tmp
 fi
 
-echo "Please flash your SD card with ./tmp/${RASPBIAN_RELEASE}.img using a tool like Etcher.io."
+echo "Please flash your SD card with ./tmp/${RASPBIAN_IMAGE} using a tool like Etcher.io."
 echo "When done, ensure the flashed SD card is mounted."
 read -p "Press [Enter] to continue."
 
